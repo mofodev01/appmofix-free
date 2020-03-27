4e0663b97ceb0198ee3b6f1ab45ba33108ff6742
mustapha <mofodev01@gmail.com>
2020-03-27 21:52:18 +0100

    uuuuuu

--- cordovaOverride.sh
+++ cordovaOverride.sh
@@ -9,7 +9,7 @@ npm install -g cordova@8.1.2
 #echo "In Appflow...using cordova-ios@5.1.1"
 #npm i cordova-ios@5.1.1
 echo "In Appflow...using cordova-ios@6.0.0-nightly.2020.2.24.4ac15525"
-npm i cordova-ios
+npm i cordova-ios@6.0.0-nightly.2020.3.27.59b8ac6b
 #echo "In Appflow...using cordova-ios@latest"
 #npm i cordova-ios@latest
 fi
